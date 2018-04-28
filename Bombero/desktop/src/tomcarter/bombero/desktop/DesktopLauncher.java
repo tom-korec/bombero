333cedd5bdbf03f7fd84522c782fd0c62ff90c9b
tkmk96 <tkmk96@gmail.com>
2018-04-28 21:05:22 +0100

    default libGDX app created

---
+++ Bombero/desktop/src/tomcarter/bombero/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tomcarter.bombero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tomcarter.bombero.Bombero;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bombero(), config);
+	}
+}
